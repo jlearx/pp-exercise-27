737499f039e30f2d35ffcc0c6d5e4ba6b2a3ce93
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-09-21 11:54:22 -0700

    Added function to get a valid move

--- src/main.py
+++ src/main.py
@@ -3,17 +3,57 @@ Created on Sep 20, 2017
 
 @author: jlearx
 '''
+COLSIZE = 3
+ROWSIZE = 3
 
-def printBoard(gameBoard):
-    size = len(gameBoard)
-    
-    for i in range(0,size):
+def printBoard(gameBoard):    
+    for i in range(0,ROWSIZE):
         print(gameBoard[i])
     
     print("")
     
 def getMove(gameBoard):
+    # Display game state
     printBoard(gameBoard)
+    
+    # Repeat until a valid move is given
+    while (True):
+        moveStr = input("Please enter a valid move as row,column: ").strip()
+        row = -1
+        col = -1        
+        
+        # Check input length
+        if (len(moveStr) < 3):
+            continue
+        
+        # Check if input contains a comma
+        cPos = moveStr.find(',')
+        
+        if ((cPos < 1) or (cPos == len(moveStr) - 1)):
+            continue
+        
+        # Parse move out of input string
+        row = moveStr[:cPos]
+        col = moveStr[cPos + 1:]
+        
+        # Make sure the row and column are numeric
+        if (row.isnumeric() and col.isnumeric()):
+            # Reduce by 1 because we start from 0
+            row = int(row) - 1
+            col = int(col) - 1
+        else:
+            continue
+        
+        # Make sure the row and column are within bounds
+        if ((row < 0) or (col < 0) or (row >= ROWSIZE) or (col >= COLSIZE)):
+            continue
+        
+        # Check if move is in a free square (0)
+        if (gameBoard[row][col] == 0):
+            return (row,col)
+        else:
+            # Show the board again
+            printBoard(gameBoard)
 
 def playGame(gameBoard):
     getMove(gameBoard)
