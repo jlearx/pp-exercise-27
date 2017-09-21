c51124ff30424361356ac590a7d9ca3a969c2c1c
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-09-21 14:28:40 -0700

    Added logic for taking turns and ending game

--- src/main.py
+++ src/main.py
@@ -55,9 +55,49 @@ def getMove(gameBoard):
             # Show the board again
             printBoard(gameBoard)
 
-def playGame(gameBoard):
-    getMove(gameBoard)
+def makeMove(gameBoard, move, p1Turn):
+    pass
 
+def checkGameOver(gameBoard):
+    gameOver = True
+    
+    for row in gameBoard:
+        if (0 in row):
+            gameOver = False
+            
+    return gameOver
+
+def playGame(gameBoard):
+    p1Turn = True
+    gameOver = False
+    
+    # Take turns and switch player at end of turn
+    while (True):
+        if (p1Turn):
+            print("Player 1's Turn")
+        else:
+            print("Player 2's Turn")
+        
+        # Get the player's move
+        move = getMove(gameBoard)
+        
+        # Update the game board with move
+        makeMove(gameBoard, move, p1Turn)
+        
+        # Check the game state
+        gameOver = checkGameOver(gameBoard)
+        
+        if (gameOver):
+            break
+        
+        # Switch current player
+        if (p1Turn):
+            p1Turn = False
+        else:
+            p1Turn = True
+
+    print("GAME OVER")
+    
 if __name__ == '__main__':
     gameBoard = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
     playGame(gameBoard)
